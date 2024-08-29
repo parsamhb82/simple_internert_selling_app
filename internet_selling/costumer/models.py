cbe338b515a5dd58c3049f556a77c47ab6b31450
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-29 09:41:33 +0330

    models completed

--- internet_selling/costumer/models.py
+++ internet_selling/costumer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from internet_app.models import Internet
 
-# Create your models here.
+class costumer(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    phone = models.CharField(max_length=20, blank=False, null=False)
+    wallet = models.IntegerField(default=100000)
+    bought_internet = models.ManyToManyField(Internet, on_delete=models.PROTECT, blank=True)
--- internet_selling/provider/models.py
+++ internet_selling/provider/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from internet_app.models import Internet
 
-# Create your models here.
+class Provider(models.Model):
+    name = models.CharField(max_length=255, bqlank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    phone = models.CharField(max_length=20, blank=False, null=False)
+    is_active = models.BooleanField(default=False)
+    internet = models.ManyToManyField(Internet, on_delete=models.PROTECT, blank=True)
